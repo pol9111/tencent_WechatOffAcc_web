d4c0abb0ee78579e951fcebcf1f86f95c5e7a85f
pol9111 <biscuit36@163.com>
2018-08-04 11:13:42 +0800

    modified spider

--- spider.py
+++ spider.py
@@ -1,4 +1,4 @@
-from four import *
+from config import *
 import requests, random, time
 
 class Spider():
@@ -10,7 +10,8 @@ class Spider():
         self.appmsg_url = APPMSG_URL
         self.table = TABLE
 
-    def search_off_acc(self):
+    @classmethod
+    def search_off_acc(cls):
         # 要爬取的公众号列表
         official_acc = ['JOKER-1943']
 
@@ -18,7 +19,7 @@ class Spider():
         for query in official_acc:
             query_id = {
                 'action': 'search_biz',
-                'token': self.token,
+                'token': TOKEN,
                 'lang': 'zh_CN',
                 'f': 'json',
                 'ajax': '1',
@@ -29,16 +30,17 @@ class Spider():
             }
 
             # 在搜索接口搜索某公众号的url构造
-            search_rs = requests.get(self.search_url, headers=self.headers, cookies=self.cookies, params=query_id)
+            search_rs = requests.get(SEARCH_URL, headers=HEADERS, cookies=COOKIES, params=query_id)
 
             # 获取公众号的fakeid
             acc_lst = search_rs.json().get('list')[0]
             FAKEID = acc_lst.get('fakeid')
             return FAKEID
 
-    def enter_off_acc(self, fakeid):
+    @classmethod
+    def enter_off_acc(cls, fakeid):
         query_id_data = {
-            'token': self.token,
+            'token': TOKEN,
             'lang': 'zh_CN',
             'f': 'json',
             'ajax': '1',
@@ -52,20 +54,21 @@ class Spider():
         }
 
         # 点击某个公众号, 进入公众号文章列表的url构造
-        appmsg_rs = requests.get(self.appmsg_url, headers=self.headers, cookies=self.cookies, params=query_id_data)
+        appmsg_rs = requests.get(APPMSG_URL, headers=HEADERS, cookies=COOKIES, params=query_id_data)
 
         # 总文章数app_msg_cnt / 5 可知要翻多少页
         max_num = appmsg_rs.json().get('app_msg_cnt')
         NUM = int(int(max_num) / 5)
         return NUM
 
-    def get_each_page(self, fakeid, num):
+    @classmethod
+    def get_each_page(cls, fakeid, num):
         begin = 0
 
         # 循环爬取某个公众号的每一页
         while num + 1 > 0:
             query_id_data = {
-                'token': self.token,
+                'token': TOKEN,
                 'lang': 'zh_CN',
                 'f': 'json',
                 'ajax': '1',
@@ -77,7 +80,7 @@ class Spider():
                 'fakeid': fakeid,
                 'type': '9'
             }
-            acc_eachpage_rs = requests.get(self.appmsg_url, headers=self.headers, cookies=self.cookies, params=query_id_data)
+            acc_eachpage_rs = requests.get(APPMSG_URL, headers=HEADERS, cookies=COOKIES, params=query_id_data)
             article_list = acc_eachpage_rs.json().get('app_msg_list')
             # 循环保存每一页文章
             for item in article_list:
@@ -90,7 +93,7 @@ class Spider():
                     '发布时间': update_time,
                 }
                 print(data)
-                self.table.update({'文章标题': title}, {'$set': data}, True)
+                TABLE.update({'文章标题': title}, {'$set': data}, True)
             num -= 1
             begin += 5
             time.sleep(15)
